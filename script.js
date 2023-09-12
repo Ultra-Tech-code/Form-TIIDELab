7da519a66a0f74ad18d75435adcb0f35d6623074
Ultra-tech-code <hizick27@gmail.com>
2023-09-12 19:05:16 +0100

    sign up done

--- script.js
+++ script.js
@@ -1,26 +1,41 @@
+function accountExist(email) {  
 //fetch userDetails from local storage
 let USERDATA = JSON.parse(localStorage.getItem("USERDATA"));
 
-let userEmail = USERDATA[accountnumber]["email"];
+USERDATA !== null && USERDATA[email] ? ` ${alert("account already exist")} ${window.location.href = "login.html" }` : null;
 
-userEmail == null ? ` ${alert("account already exist")} ${window.location.href = "login.html" }` : null;
+}
 
-
-const userData = {}
 
 function signUP(e){
   e.preventDefault();
+
   let studentData = document.forms["studentData"];
      
-  //getting the data through the formdata
-      let data = new FormData(studentData);
-     data.get("email")
+    //getting the data through the formdata
+    let data = new FormData(studentData);
 
-    data.get("password").length < 8 ? alert("Password must be at least 8 characters") : null;
+    let userinput = {
+        name: data.get("studentName"),
+        email: data.get("email"),
+        age: data.get("studentAge"),
+        password: data.get("password"),
+    }
 
-    data.get("password") != data.get("confirm-password") ? alert("Password does not match") : null;
+    accountExist(data.get("email"));  //checking if account exist
 
-    userData[data.get("email")] = data;
+    //validating the password
+    if(data.get("password").length < 8 ){
+        alert("Password must be at least 8 characters");
+        return;
+    }else if(data.get("password") != data.get("confirm-password")){
+        alert("Password does not match");
+        return;
+    }
+
+    let userData = {}
+
+    userData[data.get("email")] = userinput;
 
     //storing it in local storage
     localStorage.setItem("USERDATA",  JSON.stringify(userData));
