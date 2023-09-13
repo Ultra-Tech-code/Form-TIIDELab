2565b07d81519db2102bd7ada7d49e24e012259a
Ultra-tech-code <hizick27@gmail.com>
2023-09-13 07:24:07 +0100

    recover password done

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
+//helper function
 function accountExist(email) {  
 //fetch userDetails from local storage
 let USERDATA = JSON.parse(localStorage.getItem("USERDATA"));
 
     return USERDATA !== null && USERDATA[email] ?  true :  null;
 }
-
 
 function signUP(e){
   e.preventDefault();
@@ -26,7 +26,6 @@ function signUP(e){
         ` ${alert("account already exist")} ${window.location.href = "login.html" }`
     };  
     
-
     //validating the password
     if(data.get("password").length < 8 ){
         alert("Password must be at least 8 characters");
@@ -47,6 +46,8 @@ function signUP(e){
     alert("Account created successfully ");
     window.location.href = "login.html";
 }
+
+
 
 function login(e) {
     e.preventDefault();
@@ -80,17 +81,16 @@ function login(e) {
 }
 
 
+
 function dashboard(){
     //fetch usermail from local storage
     let usermail = localStorage.getItem("usermail")
-
 
     //fetch userDetails from local storage
     let USERDATA = JSON.parse(localStorage.getItem("USERDATA"));
     
      //get the table
     let dashboardContent = document.getElementById("dashboardContent")
-
 
     //creating a row in the table
     let row = dashboardContent.insertRow();
@@ -103,14 +103,50 @@ function dashboard(){
 
       newCell.textContent = datium;
     
-      // Check if this is not the last element
-      //prevent the password from being displayed
+      // Check if this is the last element
+      //masked the password from being displayed
         if (index === data.length - 1) {
             maskedPassword = datium.replace(/./g, '*');
             newCell.textContent = maskedPassword;
         }
     })
 
- }
+}
+
+function recover(e){
+    e.preventDefault();
+
+    let recoverData = document.forms["recoverData"];
+     
+    //getting the data through the formdata
+    let formdata = new FormData(recoverData);
+
+    let usermail = formdata.get("email")
+
+    if(accountExist(usermail) === null){
+        alert("Account does not exist, verify and try again!!");
+        return;
+    }
+
+    //fetch userDetails from local storage
+    let USERDATA = JSON.parse(localStorage.getItem("USERDATA"));
+
+    let storageData = USERDATA[usermail];
+
+    if(formdata.get("email") == storageData['email'] && formdata.get("userName") == storageData['name'] && formdata.get("userAge") == storageData['age']){
+        alert(storageData['password']);
+        recoverData.reset();
+    } else{
+        alert("Invalid Details, verify and try again.")
+    }
+}
+
+
+
+
+
+
+
+
 
 
